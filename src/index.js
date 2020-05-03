7102eddc8e1bd21f0dba926b977ba04800ece177
amine <amine.louni.dev@gmail.com>
2020-05-03 17:02:45 +0100

    stylnig

--- src/index.js
+++ src/index.js
@@ -6,12 +6,3 @@ import "./scss/main.scss";
 
 // Favicon
 import "./favicon.ico";
-
-// Web App Manifest
-import "./site.webmanifest";
-
-// robots.txt
-import "./robots.txt";
-
-// humans.txt
-import "./humans.txt";
