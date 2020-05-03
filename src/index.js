e4796c4094b38ec10778169d9c5b9529bd40b039
amine <amine.louni.dev@gmail.com>
2020-05-03 15:49:50 +0100

    init uikit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification("Hello world.");
+
 // JavaScript
 import "./scripts/main";
 
